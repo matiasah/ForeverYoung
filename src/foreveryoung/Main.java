17950e336cbafcd9891c5fca06db4cdcec5a956f
Starkkz <matiasahermosilla@gmail.com>
2017-06-04 02:49:41 -0400

    Solución pre-final

--- src/foreveryoung/Main.java
+++ src/foreveryoung/Main.java
@@ -4,23 +4,41 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.Scanner;
 
 public class Main {
-    
+
     private static class ExcepcionBase extends Exception {
+
+        private long numero;
+
+        public ExcepcionBase(long numero) {
+
+            this.numero = numero;
+
+        }
+
+        public long getNumero() {
+
+            return this.numero;
+
+        }
 
     }
 
     public static Scanner lector = new Scanner(System.in);
 
-    public static int de10(int numero, int base) throws ExcepcionBase {
+    public static long de10(long numero, long base) throws ExcepcionBase {
 
-        int potencia = 1;
-        int transformado = 0;
+        long potencia = 1;
+        long transformado = 0;
+        boolean excepcion = false;
 
         while (numero > 0) {
-            int digito = numero % base;
+            long digito = numero % base;
 
-            if (digito >= 10) {
-                throw new ExcepcionBase();
+            if (digito > 9) {
+
+                excepcion = true;
+                digito = 9;
+
             }
 
             transformado += digito * potencia;
@@ -28,101 +46,92 @@ public class Main {
             numero = (numero - digito) / base;
         }
 
+        if (excepcion) {
+            
+            throw new ExcepcionBase(transformado);
+
+        }
+
         return transformado;
 
     }
 
-    public static void mostrarBases(int numero, int limiteInferior) {
+    public static Long busquedaBinaria(long numero, long limiteInferior, long izquierda, long derecha) {
+        
+        if (limiteInferior <= 10) {
 
-        for (int i = 10; i < numero; i++) {
-
-            try {
-
-                System.out.println("Base: " + i + " numero: " + de10(numero, i));
-
-            } catch (ExcepcionBase e) {
-
-                System.out.println("Base: " + i + " error");
-
-            }
+            return numero;
 
         }
 
-    }
+        long suma = izquierda + derecha;
+        long medio = suma / 2;
 
-    public static Integer busquedaBinaria(int numero, int limiteInferior, int izquierda, int derecha) {
-        
-        int medio   =   (izquierda + derecha) / 2;
-        
-        // A mayor base, menor número
-        for (boolean error = true; error && izquierda < medio;) {
-            
-            error   =   false;
-            
-            try {
-                // Busqueda binaria a traves del límite inferior
-                int transformado = de10(numero, medio);
-                
-                if ( transformado >= limiteInferior ) {
-                    
-                    // Es mayor que el límite inferior, buscar hacia la derecha
-                    Integer busqueda = busquedaBinaria(numero, limiteInferior, (int) Math.floor( (izquierda + derecha ) * 0.5), derecha);
-                    
-                    if ( busqueda == null ) {
-                        // No se ha encontrado nada, buscar hacia la izquierda
-                        return busquedaBinaria(numero, limiteInferior, izquierda, (int) Math.floor( ( izquierda + derecha ) * 0.5) );
-                        
-                    }
-                    
-                    return busqueda;
-                    
-                } else {
-                    
-                    // Es menor que el límite inferior, buscar hacia la izquierda (aqui no se puede considerar buscar hacia la derecha)
-                    return busquedaBinaria(numero, limiteInferior, izquierda, (int) Math.ceil( (izquierda + derecha ) * 0.5 ) );
-                    
-                }
-
-            } catch (ExcepcionBase e) {
-                // Error al hacer cambio de base, apareció un digito mayor que 9
-                // Reducir el intervalo hacia la izquierda
-                error  =   true;
-                medio  =   medio - 1;
-
-            }
-
-        }
-        
-        if ( izquierda == medio ) {
+        if (izquierda == medio) {
             // Si el rango de busqueda es en un intervalo de rango 1, comparar el valor único dentro del intervalo
             try {
                 // Verificar que sea mayor que el límite inferior
-                if ( de10(numero, izquierda) >= limiteInferior ) {
-                    
+                if (de10(numero, izquierda) >= limiteInferior) {
+
                     // Es un posible resultado
                     return izquierda;
-                    
+
                 }
-                
+
             } catch (ExcepcionBase e) {
                 
+                // Si el número contiene un dígito mayor que 9, no se puede retornar
             }
+            
+            return null;
+            
+        }
+
+        // Busqueda binaria a traves del límite inferior
+        long transformado;
+
+        try {
+
+            transformado = de10(numero, medio);
+
+        } catch (ExcepcionBase e) {
+            // Si hay un dígito mayor que 9, se considera como 9.
+            transformado = e.getNumero();
             
         }
         
-        return null;
+        if (transformado >= limiteInferior) {
+
+            // Es mayor que el límite inferior, buscar hacia la derecha
+            Long busqueda = busquedaBinaria(numero, limiteInferior, medio, derecha);
+
+            if (busqueda == null) {
+                
+                // No se ha encontrado nada, buscar hacia la izquierda
+                return busquedaBinaria(numero, limiteInferior, izquierda, medio + (suma % 2));
+
+            }
+
+            return busqueda;
+
+        } else {
+
+            // Es menor que el límite inferior, buscar hacia la izquierda (aqui no se puede considerar buscar hacia la derecha)
+            return busquedaBinaria(numero, limiteInferior, izquierda, medio + (suma % 2));
+
+        }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         String Line = lector.nextLine();
         String split[] = Line.split(" ");
 
         // Se recibe un número 'y' y un límite inferior
-        int y = (int) Integer.parseInt(split[0]);
-        int limiteInferior = (int) Integer.parseInt(split[1]);
-        
+        long y = (long) Long.parseLong(split[0]);
+        long limiteInferior = (long) Long.parseLong(split[1]);
+
         System.out.println(busquedaBinaria(y, limiteInferior, 10, y));
 
     }
